5ae01e2fae214060c142960a388a21c28c33b705
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-04-03 21:54:06 +0430

    remove commented codes

--- includes/mvc/view.php
+++ includes/mvc/view.php
@@ -6,14 +6,12 @@ class view extends \lib\view
 	function _construct()
 	{
 		// define default value for global
-		// $this->include->css_main     = false;
 		$this->data->site['title']   = T_("Your Browser is DEAD!");
 		$this->data->site['desc']    = T_("We check your browser and say you are alive or dead! It's real");
 		$this->data->site['slogan']  = T_("use up-to-date browser");
 
 		$this->data->page['desc']    = T_("We check your browser and say you are alive or dead! It's real");
 
-		// $this->include->js           = false;
 		$this->data->bodyclass       = 'unselectable';
 
 		// disable common.js
