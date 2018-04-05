d7b9fd8a3120d574fb8e90bc3d83d471fc28aea2
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-04-06 03:06:59 +0430

    try to fix view

--- content/home/view.php
+++ content/home/view.php
@@ -4,86 +4,97 @@ namespace content\home;
 class view
 {
 
-	static function config()
+	public static function config()
 	{
-		return;
+		var_dump(22);
 		// Full browser_detection object
-		$this->data->browser = \lib\utility\browserDetection::browser_detection('full_assoc');
-		$this->data->name    = $this->data->browser['browser_name'];
-		$this->data->version = $this->data->browser['browser_number'];
-		$this->data->os      = $this->data->browser['os'];
+		$my_browser = \lib\utility\browserDetection::browser_detection('full_assoc');
+		$myBrowserName    = $my_browser['browser_name'];
+		$myBrowserVersion = $my_browser['browser_number'];
+		$myBrowserOs      = $my_browser['os'];
 
-		$this->data->logo    = '';
+		$myBrowserLogo    = '';
 
 		// Check the browser's HTML 5 compatibility
-		if ($this->data->browser['html_type'] == '' || $this->data->browser['html_type'] == 1 || $this->data->browser['browser_name'] == 'msie')
+		if ($my_browser['html_type'] == '' || $my_browser['html_type'] == 1 || $my_browser['browser_name'] == 'msie')
 		{
-			$this->data->old           = true;
-			$this->data->exec          = 'jpg';
-			$this->data->site['title'] = T_("You are DEAD!");
-			$this->data->page['desc']  = T_("Your browser is Dead! Please update it!");
 
-			if(isset($this->data->browser['browser_name']))
+			$myPageDesc  = T_("Your browser is Dead! Please update it!");
+
+			if(isset($my_browser['browser_name']))
 			{
-				$this->data->page['desc'] .= ' | '. $this->data->browser['browser_name'];
+				$myPageDesc .= ' | '. $my_browser['browser_name'];
 			}
 
-			if(isset($this->data->browser['browser_math_number']))
+			if(isset($my_browser['browser_math_number']))
 			{
-				$this->data->page['desc'] .= '-'. $this->data->browser['browser_math_number'];
+				$myPageDesc .= '-'. $my_browser['browser_math_number'];
 			}
 
-			if(isset($this->data->browser['os']))
+			if(isset($my_browser['os']))
 			{
-				$this->data->page['desc'] .= ' @'. $this->data->browser['os'];
+				$myPageDesc .= ' @'. $my_browser['os'];
 			}
+
+			\lib\data::old(true);
+			\lib\data::page_title(T_("You are DEAD!"));
+			\lib\data::page_desc($myPageDesc);
 		}
 		else
 		{
-			$this->data->old  = false;
-			$this->data->exec = 'png';
-			$this->data->site['title'] = T_('NO! You are Alive!');
-			$this->data->page['desc']    = T_("Your browser is up-to-date! Please enjoy using it!");
+			\lib\data::old(false);
+			\lib\data::page_title(T_('NO! You are Alive!'))
+			\lib\data::page_desc(T_("Your browser is up-to-date! Please enjoy using it!"));
 		}
 
 		// By default for firefox "browser_name" is "gecko". We set it to "firefox"
-		if ($this->data->browser['browser_name'] == 'gecko') {
-			$this->data->browser['browser_name'] = 'firefox';
-			$this->data->name = T_('Firefox');
+		if ($my_browser['browser_name'] == 'gecko')
+		{
+			$my_browser['browser_name'] = 'firefox';
+			$myBrowserName = T_('Firefox');
 		}
 
 		// By default for chrome "browser_name" is "chrome". We set it to "google chrome"
-		if ($this->data->browser['browser_name'] == 'chrome') {
-			$this->data->name = T_('Google Chrome');
+		if ($my_browser['browser_name'] == 'chrome')
+		{
+			$myBrowserName = T_('Google Chrome');
 		}
 
 		// By default for internet explorer "browser_name" is "msie". We set it to "internet explorer"
-		if ($this->data->browser['browser_name'] == 'msie') {
-			$this->data->name = T_('Internet Explorer');
+		if ($my_browser['browser_name'] == 'msie')
+		{
+			$myBrowserName = T_('Internet Explorer');
 		}
 
-		if ($this->data->browser['browser_name'] == 'edge') {
-			$this->data->name = T_('Edge');
+		if ($my_browser['browser_name'] == 'edge')
+		{
+			$myBrowserName = T_('Edge');
 		}
 
-		if ($this->data->browser['browser_name'] == 'safari') {
-			$this->data->name = T_('Safari');
+		if ($my_browser['browser_name'] == 'safari')
+		{
+			$myBrowserName = T_('Safari');
 		}
 
-		if ($this->data->browser['os'] == 'nt') {
-			$this->data->os = T_('windows');
+		if ($my_browser['os'] == 'nt')
+		{
+			$myBrowserOs = T_('windows');
 		}
 
-		if ($this->data->browser['os'] == 'lin') {
-			if ($this->data->browser['ua_type'] == 'mobile') {
-				$this->data->os = T_('android');
-			} else {
-				$this->data->os = T_('linux');
+		if ($my_browser['os'] == 'lin')
+		{
+			if ($my_browser['ua_type'] == 'mobile')
+			{
+				$myBrowserOs = T_('android');
+			}
+			else
+			{
+				$myBrowserOs = T_('linux');
 			}
 		}
 
 		// Message 1
-		$this->data->message1 = ucwords(T_($this->data->name)) . " " . $this->data->version;
+		$myMsg1 = ucwords(T_($myBrowserName)) . " " . $myBrowserVersion;
 
 		$browsers = array(
 			"chrome"  => 64.0,
@@ -95,33 +106,42 @@ class view
 		);
 
 		// Message 2
-		if (isset($browsers[$this->data->browser['browser_name']]))
+		if (isset($browsers[$my_browser['browser_name']]))
 		{
 
-			if ($this->data->browser['browser_math_number'] < $browsers[$this->data->browser['browser_name']])
+			if ($my_browser['browser_math_number'] < $browsers[$my_browser['browser_name']])
 			{
-				$this->data->message2 = '<b>'.$browsers[$this->data->browser['browser_name']] . "</b> " . T_("is the latest version");
+				$myMsg2 = '<b>'.$browsers[$my_browser['browser_name']] . "</b> " . T_("is the latest version");
 			}
 			else
 			{
-				$this->data->message2 = T_("Hooray! You are using the latest version");
+				$myMsg2 = T_("Hooray! You are using the latest version");
 			}
 
-			$this->data->logo = $this->data->browser['browser_name'];
+			$myBrowserLogo = $my_browser['browser_name'];
 
 		}
 		else
 		{
-			$this->data->message2 = T_("Sorry! We have not enough information about your browser");
-			$this->data->logo     = T_("undefined");
+			$myMsg2 = T_("Sorry! We have not enough information about your browser");
+			$myBrowserLogo     = 'undefined';
 		}
 
-		if($this->data->browser['browser_name'] == 'msie')
+		if($my_browser['browser_name'] == 'msie')
 		{
-			$this->data->message2 = T_("IE is DIE!");
+			$myMsg2 = T_("IE is DIE!");
 		}
 
-		// var_dump($this->data->browser);
+		\lib\data::browser($my_browser);
+		\lib\data::name($myBrowserName);
+		\lib\data::version($myBrowserVersion);
+		\lib\data::os($myBrowserOs);
+		\lib\data::logo($myBrowserLogo);
+		\lib\data::message1($myMsg1);
+		\lib\data::message2($myMsg2);
+
+
+		// var_dump($my_browser);
 	}
 }
 ?>
