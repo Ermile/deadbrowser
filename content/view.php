231bea824b43a0dbf6a8b77effb0b28651150bfc
Javad Evazzadeh Kakroudi <J.Evazzadeh@live.com>
2018-04-06 03:54:38 +0430

    fix site title

--- content/view.php
+++ content/view.php
@@ -5,7 +5,7 @@ class view
 {
 	public static function config()
 	{
-		\lib\data::site_title(T_("Your Browser is DEAD!"));
+		\lib\data::site_title(T_("DeadBrowser"));
 		\lib\data::site_desc(T_("We check your browser and say you are alive or dead! It's real"));
 		\lib\data::site_slogan(T_("use up-to-date browser"));
 
