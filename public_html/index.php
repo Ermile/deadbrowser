4ccef6d811003885ccd7dc761b12e37fbe0f04cb
Javad Evazzadeh <J.Evazzadeh@live.com>
2018-04-21 17:17:12 +0430

    fix index

--- public_html/index.php
+++ public_html/index.php
@@ -3,14 +3,17 @@
  @ In the name Of Allah
 **/
 
-// if Saloos exist, require it else show related error message
-if ( file_exists( '../../dash/autoload.php') )
+// if Dash exist, require it else show related error message
+if ( file_exists( '../dash/autoload.php') )
+{
+	require_once( '../dash/autoload.php');
+}
+elseif ( file_exists( '../../dash/autoload.php') )
 {
 	require_once( '../../dash/autoload.php');
 }
 else
 {   // A config file doesn't exist
-	echo( "<p>We can't find <b>Saloos</b>! Please contact administrator!</p>" );
-	exit();
+	exit("<p>We can't find <b>Dash</b>! Please contact administrator!</p>");
 }
 ?>
