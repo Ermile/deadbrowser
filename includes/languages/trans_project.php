90faa37f2adfbafaad02150b70ce8098a7f20637
Javad Evazzadeh <J.Evazzadeh@live.com>
2019-01-11 13:24:35 +0330

    update trans

---
+++ includes/languages/trans_project.php
@@ -0,0 +1,30 @@
+<?php
+class twigTransTerms
+{
+ private function transtext()
+ {
+
+	//--------------------------\htdocs\Projects\deadbrowser\content\home\display.html
+	echo T_("made by");                                                               // Line 8
+	echo T_("Ermile is our brand");                                                   // Line 8
+	echo T_("Ermile");                                                                // Line 8
+	echo T_("Try a different browser");                                               // Line 73
+	echo T_("Internet Explorer ");                                                    // Line 78
+	echo T_("Mozilla Firefox");                                                       // Line 84
+	echo T_("Google Chrome");                                                         // Line 89
+	echo T_("Opera");                                                                 // Line 94
+	echo T_("Apple Safari");                                                          // Line 100
+
+	//\htdocs\Projects\deadbrowser\public_html\static\siftal\fonts\siftal\icons-reference.html
+
+	//---------\htdocs\Projects\deadbrowser\public_html\static\siftal\js\error_page.js
+
+	//\htdocs\Projects\deadbrowser\public_html\static\siftal\js\highcharts\highcharts.min.js
+
+	//------\htdocs\Projects\deadbrowser\public_html\static\siftal\js\highlight.min.js
+
+	//---------\htdocs\Projects\deadbrowser\public_html\static\siftal\js\siftal.min.js
+
+ }
+}
+?>
